98981cf07f5fc16c4b5e18ae84db2713d4308fb4
Gmirzoyan <Garry.mirzoyan@aldautomotive.ru>
2025-02-23 01:20:24 +0300

    1 попытка

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+git@github.com:M-Garry/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,42 @@
-// @todo: Темплейт карточки
+// Шаблон карточки, который будет использоваться для создания карточек
+const Template = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узел, куда будут добавляться карточки
+const cardPlace = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(img, title) {
+  // Создаем копию шаблона карточки
+  const card = Template.querySelector('.places__item').cloneNode(true);
+  
+  // Устанавливаем путь к изображению и alt текст
+  card.querySelector('.card__image').src = img;
+  card.querySelector('.card__image').alt = `Картинка с изображением ${title}`;
+  
+  // Устанавливаем текст заголовка карточки
+  card.querySelector('.card__title').textContent = title;
+  
+  // Добавляем обработчик события клика на кнопку удаления карточки
+  card.querySelector(".card__delete-button").addEventListener('click', deleteCard);
+  
+  // Возвращаем готовую карточку
+  return card;
+}
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function deleteCard(event) {
+  // Находим карточку, на которую был кликнут delete button
+  const cardForDelete = event.target.closest('.places__item');
+  
+  // Удаляем карточку
+  cardForDelete.remove();
+}
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
+// initialCards - это массив карточек, которые были определены в файле cards.js
+initialCards.forEach((card) => {
+  // Создаем карточку с помощью функции createCard
+  // и добавляем ее на страницу
+  cardPlace.append(createCard(card.link, card.name));
+});
+
